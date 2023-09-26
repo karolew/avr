10a1e84bae9484f0b1b0cfbd324332873e46f900
karolew <fratczak.karolew@gmail.com>
2023-09-26 21:21:50 +0200

    Avr LCD 44780 lib

---
+++ AVR-LCD/lcd_44780.c
@@ -0,0 +1,223 @@
+#include <avr/io.h>
+#include <avr/delay.h>
+
+#include "lcd_44780.h"
+
+
+void set_data_port_out(void)
+{
+	DDR(LCD_D4_PORT) |= (1<<LCD_D4_PIN);
+	DDR(LCD_D5_PORT) |= (1<<LCD_D5_PIN);
+	DDR(LCD_D6_PORT) |= (1<<LCD_D6_PIN);
+	DDR(LCD_D7_PORT) |= (1<<LCD_D7_PIN);
+}
+
+
+void set_data_port_in(void)
+{
+	DDR(LCD_D4_PORT) &= ~(1<<LCD_D4_PIN);
+	DDR(LCD_D5_PORT) &= ~(1<<LCD_D5_PIN);
+	DDR(LCD_D6_PORT) &= ~(1<<LCD_D6_PIN);
+	DDR(LCD_D7_PORT) &= ~(1<<LCD_D7_PIN);
+}
+
+
+void set_control_port_out(void)
+{
+	DDR(LCD_E_PORT) |= (1<<LCD_E_PIN);
+	DDR(LCD_RS_PORT) |= (1<<LCD_RS_PIN);
+	#if LCD_RW
+		DDR(LCD_RW_PORT) |= (1<<LCD_RW_PIN);
+	#endif
+}
+
+
+void set_control_port_in(void)
+{
+	DDR(LCD_E_PORT) &= ~(1<<LCD_E_PIN);
+	DDR(LCD_RS_PORT) &= ~(1<<LCD_RS_PIN);
+	#if LCD_RW
+		DDR(LCD_RW_PORT) &= ~(1<<LCD_RW_PIN);
+	#endif
+}
+
+
+#if LCD_RW
+	static inline uint8_t lcd_read_half_byte(void)
+	{
+		uint8_t data = 0;
+		SET_E;
+		if (PIN(LCD_D4_PORT) & (1<<LCD_D4_PIN)) data |= (1<<0);
+		if (PIN(LCD_D5_PORT) & (1<<LCD_D5_PIN)) data |= (1<<1);
+		if (PIN(LCD_D6_PORT) & (1<<LCD_D6_PIN)) data |= (1<<2);
+		if (PIN(LCD_D7_PORT) & (1<<LCD_D7_PIN)) data |= (1<<3);
+		CLR_E;
+		return data;
+	}
+
+	uint8_t lcd_read_byte(void)
+	{
+		set_data_port_in();
+		SET_RW;
+		uint8_t data = 0;
+		data = (lcd_read_half_byte()<<4);
+		data |= lcd_read_half_byte();
+		return data;
+	}
+
+	uint8_t lcd_read_flag(void)
+	{
+		CLR_RS;
+		return lcd_read_byte();
+	}
+
+	void lcd_check_busy(void)
+	{
+		while(lcd_read_flag() & 0x80);
+	}
+#endif
+
+
+static inline void lcd_write_half_byte(uint8_t data)
+{
+	SET_E;
+	if (data & (1<<0)) PORT(LCD_D4_PORT) |= (1<<LCD_D4_PIN);
+	else PORT(LCD_D4_PORT) &= ~(1<<LCD_D4_PIN);
+	if (data & (1<<1)) PORT(LCD_D5_PORT) |= (1<<LCD_D5_PIN);
+	else PORT(LCD_D5_PORT) &= ~(1<<LCD_D5_PIN);
+	if (data & (1<<2)) PORT(LCD_D6_PORT) |= (1<<LCD_D6_PIN);
+	else PORT(LCD_D6_PORT) &= ~(1<<LCD_D6_PIN);
+	if (data & (1<<3)) PORT(LCD_D7_PORT) |= (1<<LCD_D7_PIN);
+	else PORT(LCD_D7_PORT) &= ~(1<<LCD_D7_PIN);
+	CLR_E;
+}
+
+
+void lcd_write_byte(uint8_t data)
+{
+	#if LCD_RW
+		CLR_RW;
+	#endif
+
+	set_data_port_out();
+	lcd_write_half_byte(data>>4);
+	lcd_write_half_byte(data);
+
+	#if LCD_RW
+		lcd_check_busy();
+	#else
+		_delay_us(120);
+	#endif
+}
+
+
+void lcd_write_cmd(uint8_t cmd)
+{
+	CLR_RS;
+	lcd_write_byte(cmd);
+}
+
+
+void lcd_write_char(char data)
+{
+	SET_RS;
+	lcd_write_byte(data);
+}
+
+
+void lcd_write_text(char* text)
+{
+	while(*text)
+	{
+		lcd_write_char(*text++);
+	}
+}
+
+
+void lcd_cursor(uint8_t x, uint8_t y)
+{
+	switch(y)
+	{
+		case 0:
+			lcd_write_cmd( LCDC_SET_DDRAM | (LCD_DDRAM_LINE_1 + x) );
+			break;
+	#if LCD_ROW > 1
+		case 1:
+			lcd_write_cmd( LCDC_SET_DDRAM | (LCD_DDRAM_LINE_2 + x) );
+			break;
+	#endif
+	#if LCD_ROW > 2
+		case 2:
+			lcd_write_cmd( LCDC_SET_DDRAM | (LCD_DDRAM_LINE_3 + x) );
+			break;
+		case 3:
+			lcd_write_cmd( LCDC_SET_DDRAM | (LCD_DDRAM_LINE_4 + x) );
+			break;
+	#endif
+	}
+}
+
+void lcd_cursor_on(void)
+{
+	lcd_write_cmd( LCD_ONOFF | LCD_DISP_ON | LCD_CURSOR_ON );
+	#if LCD_RW == 0
+		_delay_ms(4.1);
+	#endif
+}
+
+void lcd_cursor_off(void)
+{
+	lcd_write_cmd( LCD_ONOFF | LCD_DISP_ON | LCDC_CURSOR_OFF );
+	#if LCD_RW == 0
+		_delay_ms(4.1);
+	#endif
+}
+
+void lcd_cls(void)
+{
+	lcd_write_cmd( LCD_CLEAR );
+	#if LCD_RW == 0
+		_delay_ms(4.1);
+	#endif
+}
+
+void lcd_shift_text(char* text)
+{
+/*	0,  1,  2  ... 17,  18,  19
+	64, 65, 66 ... 81,  82,  83
+	20, 21, 22 ... 37,  38,  39
+	84, 85, 86 ... 101, 102, 103*/
+	uint8_t LINE_21_DIFF = 0x19;
+	uint8_t text_size = sizeof(*text);
+	// TODO
+}
+
+
+void lcd_init(void)
+{
+	set_data_port_out();
+	set_control_port_out();
+
+	_delay_ms(15);
+
+	CLR_E;
+	CLR_RS;
+	#if LCD_RW
+		CLR_RW;
+	#endif
+
+	lcd_write_half_byte(0x03);
+	_delay_ms(4.1);
+	lcd_write_half_byte(0x03);
+	_delay_us(100);
+	lcd_write_half_byte(0x03);
+	_delay_us(100);
+	lcd_write_half_byte(0x02);
+	_delay_us(100);
+
+	lcd_write_cmd( LCD_FUNC | LCD_4_BIT | LCDC_TWO_LINE | LCDC_FONT_5x10 );
+	lcd_write_cmd( LCD_ONOFF | LCD_DISP_ON );
+	_delay_ms(4.1);
+	lcd_write_cmd( LCDC_ENTRY_MODE | LCD_EM_SHIFT_CURSOR | LCD_EM_RIGHT );
+	lcd_cls();
+}
---
+++ AVR-LCD/lcd_44780.h
@@ -0,0 +1,79 @@
+#ifndef _d_lcd
+#define _d_lcd
+
+#include "lcd_cfg_44780.h"
+
+
+/*LCD type*/
+#if ( ( LCD_ROW == 4 ) && ( LCD_COL == 20 ) )
+	#define LCD_DDRAM_LINE_1	0x00
+	#define LCD_DDRAM_LINE_2	0x40
+	#define LCD_DDRAM_LINE_3	0x14
+	#define LCD_DDRAM_LINE_4	0x54
+#else
+	#define LCD_DDRAM_LINE_1	0x00
+	#define LCD_DDRAM_LINE_2	0x40
+	#define LCD_DDRAM_LINE_3	0x10
+	#define LCD_DDRAM_LINE_4	0x50
+#endif
+
+
+/*Port macros*/
+#define PORT(x) SPORT(x)
+#define SPORT(x) (PORT##x)
+#define PIN(x) SPIN(x)
+#define SPIN(x) (PIN##x)
+#define DDR(x) SDDR(x)
+#define SDDR(x) (DDR##x)
+
+
+/*Control macros*/
+#define SET_E PORT(LCD_E_PORT) |= (1<<LCD_E_PIN)
+#define CLR_E PORT(LCD_E_PORT) &= ~(1<<LCD_E_PIN)
+#define SET_RS PORT(LCD_RS_PORT) |= (1<<LCD_RS_PIN)
+#define CLR_RS PORT(LCD_RS_PORT) &= ~(1<<LCD_RS_PIN)
+#if LCD_RW
+	#define SET_RW PORT(LCD_RW_PORT) |= (1<<LCD_RW_PIN)
+	#define CLR_RW PORT(LCD_RW_PORT) &= ~(1<<LCD_RW_PIN)
+#endif
+
+
+/*44780 instructions*/
+#define LCD_CLEAR					0x01
+#define LCD_HOME					0x02
+#define LCDC_ENTRY_MODE				0x04
+	#define LCD_EM_SHIFT_CURSOR		0x00
+	#define LCD_EM_SHIFT_DISPLAY	0x01
+	#define LCD_EM_LEFT		   		0x00
+	#define LCD_EM_RIGHT			0x02
+#define LCD_ONOFF					0x08
+	#define LCD_DISP_ON				0x04
+	#define LCD_CURSOR_ON			0x02
+	#define LCDC_CURSOR_OFF			0x00
+	#define LCDC_BLINK_ON			0x01
+	#define LCDC_BLINK_OFF			0x00
+#define LCD_SHIFT					0x10
+	#define LCDC_SHIFT_DISP			0x08
+	#define LCDC_SHIFT_CURSOR		0x00
+	#define LCDC_SHIFT_RIGHT		0x04
+	#define LCDC_SHIFT_LEFT			0x00
+#define LCD_FUNC					0x20
+	#define LCD_8_BIT				0x10
+	#define LCD_4_BIT				0x00
+	#define LCDC_TWO_LINE			0x08
+	#define LCDC_FONT_5x10			0x04
+	#define LCDC_FONT_5x7			0x00
+#define LCDC_SET_CGRAM				0x40
+#define LCDC_SET_DDRAM				0x80
+
+
+/*Public LCD functions*/
+void lcd_init(void);
+void lcd_cursor(uint8_t x, uint8_t y);
+void lcd_write_text(char* text);
+void lcd_write_char(char data);
+void lcd_cls(void);
+void lcd_cursor_on(void);
+void lcd_cursor_off(void);
+
+#endif

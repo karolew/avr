ff0d40526e6f8ac2866243ceddb17c03032f8645
karolew <fratczak.karolew@gmail.com>
2023-10-02 19:22:43 +0200

    Code formatting

--- AVR-LCD/lcd_44780.h
+++ AVR-LCD/lcd_44780.h
@@ -39,32 +39,32 @@
 
 
 /*44780 instructions*/
-#define LCD_CLEAR					0x01
-#define LCD_HOME					0x02
-#define LCDC_ENTRY_MODE				0x04
-	#define LCD_EM_SHIFT_CURSOR		0x00
-	#define LCD_EM_SHIFT_DISPLAY	0x01
-	#define LCD_EM_LEFT		   		0x00
-	#define LCD_EM_RIGHT			0x02
-#define LCD_ONOFF					0x08
-	#define LCD_DISP_ON				0x04
-	#define LCD_CURSOR_ON			0x02
-	#define LCDC_CURSOR_OFF			0x00
-	#define LCDC_BLINK_ON			0x01
-	#define LCDC_BLINK_OFF			0x00
-#define LCD_SHIFT					0x10
-	#define LCDC_SHIFT_DISP			0x08
-	#define LCDC_SHIFT_CURSOR		0x00
-	#define LCDC_SHIFT_RIGHT		0x04
-	#define LCDC_SHIFT_LEFT			0x00
-#define LCD_FUNC					0x20
-	#define LCD_8_BIT				0x10
-	#define LCD_4_BIT				0x00
-	#define LCDC_TWO_LINE			0x08
-	#define LCDC_FONT_5x10			0x04
-	#define LCDC_FONT_5x7			0x00
-#define LCDC_SET_CGRAM				0x40
-#define LCDC_SET_DDRAM				0x80
+#define LCD_CLEAR                   0x01
+#define LCD_HOME                    0x02
+#define LCDC_ENTRY_MODE             0x04
+    #define LCD_EM_SHIFT_CURSOR     0x00
+    #define LCD_EM_SHIFT_DISPLAY    0x01
+    #define LCD_EM_LEFT             0x00
+    #define LCD_EM_RIGHT            0x02
+#define LCD_ONOFF                   0x08
+    #define LCD_DISP_ON             0x04
+    #define LCD_CURSOR_ON           0x02
+    #define LCDC_CURSOR_OFF         0x00
+    #define LCDC_BLINK_ON           0x01
+    #define LCDC_BLINK_OFF          0x00
+#define LCD_SHIFT                   0x10
+    #define LCDC_SHIFT_DISP         0x08
+    #define LCDC_SHIFT_CURSOR       0x00
+    #define LCDC_SHIFT_RIGHT        0x04
+    #define LCDC_SHIFT_LEFT         0x00
+#define LCD_FUNC                    0x20
+    #define LCD_8_BIT               0x10
+    #define LCD_4_BIT               0x00
+    #define LCDC_TWO_LINE           0x08
+    #define LCDC_FONT_5x10          0x04
+    #define LCDC_FONT_5x7           0x00
+#define LCDC_SET_CGRAM              0x40
+#define LCDC_SET_DDRAM              0x80
 
 
 /*Public LCD functions*/
--- AVR-LCD/lcd_cfg_44780.h
+++ AVR-LCD/lcd_cfg_44780.h
@@ -2,40 +2,40 @@
 #define lcd_cfg_44780
 
 /* Define LCD type
- * 			LCD_COL 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20
- * LCD_ROW   		__________________________________________________________
- * 		  1        |____________________1x16______________________|           |
- * 		  2        |____________________2x16______________________|           |
- * 		  3        |                                                          |
- * 		  4        |____________________________4x20__________________________|
+ *          LCD_COL 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20
+ * LCD_ROW          __________________________________________________________
+ *        1        |____________________1x16______________________|           |
+ *        2        |____________________2x16______________________|           |
+ *        3        |                                                          |
+ *        4        |____________________________4x20__________________________|
  */
-#define LCD_ROW				4
-#define LCD_COL				20
+#define LCD_ROW             4
+#define LCD_COL             20
 
 /* Define uC ports and pins for LCD control
  * RS	Register Select
  * E	Enable
  * RW	Read / Write*/
-#define LCD_E_PORT			B
-#define LCD_E_PIN			0
-#define LCD_RS_PORT			B
-#define LCD_RS_PIN			1
+#define LCD_E_PORT          B
+#define LCD_E_PIN           0
+#define LCD_RS_PORT         B
+#define LCD_RS_PIN          1
 /* RW 1 - Read Option enabled. LCD RW pin must be connected to defined uC port.
  * RW 0 - Read Option disabled. LCD RW pin must be connected to 0V.*/
-#define LCD_RW 1
+#define LCD_RW              1
 #if LCD_RW
-	#define LCD_RW_PORT		B
-	#define LCD_RW_PIN		2
+  #define LCD_RW_PORT       B
+  #define LCD_RW_PIN        2
 #endif
 
 /* Define uC ports and pins for LCD data*/
-#define LCD_D4_PORT			D
-#define LCD_D4_PIN			2
-#define LCD_D5_PORT			D
-#define LCD_D5_PIN			3
-#define LCD_D6_PORT			D
-#define LCD_D6_PIN			4
-#define LCD_D7_PORT			D
-#define LCD_D7_PIN			5
+#define LCD_D4_PORT         D
+#define LCD_D4_PIN          2
+#define LCD_D5_PORT         D
+#define LCD_D5_PIN          3
+#define LCD_D6_PORT         D
+#define LCD_D6_PIN          4
+#define LCD_D7_PORT         D
+#define LCD_D7_PIN          5
 
 #endif

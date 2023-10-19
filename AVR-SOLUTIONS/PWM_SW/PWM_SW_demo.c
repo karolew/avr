61992d5abd9bcb0aba9ee36fb2a1d18453a7f15c
karolew <fratczak.karolew@gmail.com>
2023-10-19 21:32:22 +0200

    SW PWM configurable PWM channels from 1 to 4

--- AVR-SOLUTIONS/PWM_SW/PWM_SW_demo.c
+++ AVR-SOLUTIONS/PWM_SW/PWM_SW_demo.c
@@ -1,3 +1,8 @@
+#ifdef F_CPU
+#undef F_CPU
+#define F_CPU 16000000UL
+#endif
+
 #include <util/delay.h>
 
 #include <avr/io.h>
@@ -6,17 +11,26 @@
 
 #include "PWM_SW_demo.h"
 
-volatile uint8_t cnt_pwm_0;
-volatile uint8_t cnt_pwm_1;
+#if SW_PWM_CHANNELS >= 1
+    volatile uint8_t cnt_pwm_0;
+#endif
+#if SW_PWM_CHANNELS >= 2
+    volatile uint8_t cnt_pwm_1;
+#endif
+#if SW_PWM_CHANNELS >= 3
+    volatile uint8_t cnt_pwm_2;
+#endif
+#if SW_PWM_CHANNELS >= 4
+    volatile uint8_t cnt_pwm3;
+#endif
 
-
-void pwm_timer_init()
+void pwm_timer_init(uint8_t timer_ocr)
 {
     /*
      * TIMER2 CONFIGURATION
      */
     /* Divide CPU frequency by 200 */
-    OCR2A = 200;
+    OCR2A = timer_ocr;
 
     /* Timer2 CTC mode*/
     TCCR2A |= (1<<WGM21);
@@ -31,26 +45,50 @@ void pwm_timer_init()
 
 void pwm_port_init()
 {
-    DDRPWM |= (1<<PWM0) | (1<<PWM1);
-    PORTPWM |= (1<<PWM0) | (1<<PWM1);
+    #if SW_PWM_CHANNELS >= 1
+        DDRPWM |= PWM0;
+        PORTPWM |= PWM0;
+    #endif
+    #if SW_PWM_CHANNELS >= 2
+        DDRPWM |= PWM1;
+        PORTPWM |= PWM1;
+    #endif
+    #if SW_PWM_CHANNELS >= 3
+        DDRPWM |= PWM2;
+        PORTPWM |= PWM2;
+    #endif
+    #if SW_PWM_CHANNELS >= 4
+        DDRPWM |= PWM3;
+        PORTPWM |= PWM3;
+    #endif
 }
 
 
 ISR( TIMER2_COMPA_vect)
 {
     static uint8_t cnt;
-
-    if(cnt >= cnt_pwm_0) PORTPWM |= (1<<PWM0);
-    else PORTPWM &= ~(1<<PWM0);
-
-    if(cnt >= cnt_pwm_1) PORTPWM |= (1<<PWM1);
-    else PORTPWM &= ~(1<<PWM1);
+    #if SW_PWM_CHANNELS >= 1
+        if(cnt >= cnt_pwm_0) PORTPWM |= PWM0;
+        else PORTPWM &= ~PWM0;
+    #endif
+    #if SW_PWM_CHANNELS >= 2
+        if(cnt >= cnt_pwm_1) PORTPWM |= PWM1;
+        else PORTPWM &= ~PWM1;
+    #endif
+    #if SW_PWM_CHANNELS >= 3
+        if(cnt >= cnt_pwm_2) PORTPWM |= PWM2;
+        else PORTPWM &= ~PWM2;
+    #endif
+    #if SW_PWM_CHANNELS >= 4
+        if(cnt >= cnt_pwm_3) PORTPWM |= PWM3;
+        else PORTPWM &= ~PWM3;
+    #endif
 }
 
 
 int PWM_SW_demo()
 {
-    pwm_timer_init();
+    pwm_timer_init(200);
     pwm_port_init();
     sei();
 
@@ -60,8 +98,8 @@ int PWM_SW_demo()
         for(uint8_t i = 0 ; i < 255 ; i++)
         {
             cnt_pwm_0 = i;
-            cnt_pwm_1 = i % 30;
-            _delay_ms(30);
+            cnt_pwm_1 = i % 4;
+            _delay_ms(12);
         }
 
     }
--- AVR-SOLUTIONS/PWM_SW/PWM_SW_demo.h
+++ AVR-SOLUTIONS/PWM_SW/PWM_SW_demo.h
@@ -1,17 +1,38 @@
 #ifndef PWM_SW_PWM_SW_DEMO_H_
 #define PWM_SW_PWM_SW_DEMO_H_
 
-#define PWM0    PD6
-#define PWM1    PD7
+#include <avr/io.h>
+
+/*Configurable from 1 - 4*/
+#define SW_PWM_CHANNELS 2
+
+/*Configure Ports and Pins for PWM. This version support only one Port*/
+#if SW_PWM_CHANNELS >= 1
+    #define PPWM0    PD4
+    #define PWM0    (1<<PPWM0)
+    extern volatile uint8_t cnt_pwm_0;
+#endif
+#if SW_PWM_CHANNELS >= 2
+    #define PPWM1    PD5
+    #define PWM1    (1<<PPWM1)
+    extern volatile uint8_t cnt_pwm_1;
+#endif
+#if SW_PWM_CHANNELS >= 3
+    #define PPWM2    PD6
+    #define PWM2    (1<<PPWM2)
+    extern volatile uint8_t cnt_pwm_2;
+#endif
+#if SW_PWM_CHANNELS >= 4
+    #define PPWM3    PD7
+    #define PWM3    (1<<PPWM3)
+    extern volatile uint8_t cnt_pwm3;
+#endif
 
 #define DDRPWM  DDRD
 #define PORTPWM PORTD
 
-/*Veriables to control SW PWM channels*/
-//extern volatile uint8_t cnt_pwm_0;
-//extern volatile uint8_t cnt_pwm_1;
-
-void pwm_timer_init();
+void pwm_timer_init(uint8_t timer_ocr);
 void pwm_port_init();
+int PWM_SW_demo();
 
 #endif /* PWM_SW_PWM_SW_DEMO_H_ */
